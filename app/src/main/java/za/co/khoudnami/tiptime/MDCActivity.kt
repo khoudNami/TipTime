7d976350940a142700d485712eb278e1ea7a8e0c
Sibonelo Shezi <siboneloshezi50@gmail.com>
2021-06-14 12:34:31 +0200

    styles and themes.

--- app/src/main/java/za/co/khoudnami/tiptime/MDCActivity.kt
+++ app/src/main/java/za/co/khoudnami/tiptime/MDCActivity.kt
@@ -1,11 +1,32 @@
 package za.co.khoudnami.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
 class MDCActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mdcactivity)
+
+        val costOfServiceEditText: TextInputEditText =
+            findViewById<TextInputEditText>(R.id.cost_of_service_edit_text)
+
+        costOfServiceEditText.setOnKeyListener { v, keyCode, _ -> handleKeyEvent(v, keyCode) }
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
